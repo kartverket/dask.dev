81a6b50c33c66f05cea9203bbeef2ca06b122a9f
Jørgen Granseth <jorgen.granseth@bekk.no>
2025-04-23 12:57:05 +0200

    Fiks bygg & README (#5)
    * Fjern ubrukte imports
    
    * Legg til nye steg i README
    
    * @kartverket -> @internal
    
    * Rydd i README

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,6 +4,6 @@ import { createBackend } from '@backstage/backend-defaults';
 const backend = createBackend();
 
 
-backend.add(import('@kartverket/backstage-plugin-dask-onboarding-backend'));
+backend.add(import('@internal/backstage-plugin-dask-onboarding-backend'));
 
 backend.start();
